858d061f27c209393eb66ea86d113f8dd51a225b
Aleksandar Sabev <aleksandars05@abv.bg>
2026-01-10 13:59:30 +0200

    Initial commit

---
+++ BookMyCut/src/main/java/org/example/bookmycut/BookMyCutApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookmycut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyCutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyCutApplication.class, args);
+    }
+
+}
---
+++ BookMyCut/src/test/java/org/example/bookmycut/BookMyCutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookmycut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyCutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
